689d0c380a8c3645dc040506f2a31c3bc35f23af
Nomor <thirimyat68@gmail.com>
2024-05-12 12:49:31 +0630

    working on admin

--- src/Components/Adoption/Adoption.jsx
+++ src/Components/Adoption/Adoption.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from 'react'
 import './Adoption.css'
-import mochi from '../../Assets/images/adopt_Mochi.jpg'
+//import mochi from '../../Assets/images/adopt_Mochi.jpg'
 
 const Adoption = () => {
+
+  const [pet,setPet]= useState([]);
+
+  const fetchInfo = async()=>{
+    await fetch('http://localhost:4000/pets')
+    .then((res)=>res.json())
+    .then((data)=>{setPet(data)});
+  }
+
+  useEffect(() => {
+    fetchInfo();
+  },[])
+
+
+
   return (
     <div className='adoption'>
         <div className="adoption-headline">
@@ -10,111 +26,49 @@ const Adoption = () => {
         </div>
         <p className='filter'>Filters :</p>
         <div className="adoption-filter-wrapper">
-        <div className="adoption-filter">
-          <label htmlFor="">Type</label>
-          <select name="" id="" >
-            <option value="dog">dog</option>
-            <option value="cat">cat</option>
-            <option value="rabbit">rabbit</option>
-            <option value="bird">bird</option>
-          </select>
+          <div className="adoption-filter">
+            <label htmlFor="">Type</label>
+            <select name="" id="" >
+              <option value="dog">dog</option>
+              <option value="cat">cat</option>
+              <option value="rabbit">rabbit</option>
+              <option value="bird">bird</option>
+            </select>
+          </div>
+          <div className="adoption-filter">
+            <label htmlFor="">Age</label>
+            <select name="" id="" >
+              <option value="under one">under one</option>
+              <option value="one to three">one to three</option>
+              <option value="older than three">older than three</option>
+            </select>
+          </div>
+          <div className="adoption-filter">
+            <label htmlFor="">Gender</label>
+            <select name="" id="" >
+              <option value="male">male</option>
+              <option value="female">female</option>
+              <option value="any">any</option>
+            </select>
+          </div>
+          <div className="adoption-filter">
+            <label htmlFor="">Color</label>
+            <select name="" id="" >
+              <option value="grey">grey</option>
+              <option value="white">white</option>
+              <option value="orange">orange</option>
+              <option value="black">black</option>
+            </select>
+          </div>
         </div>
-        <div className="adoption-filter">
-          <label htmlFor="">Age</label>
-          <select name="" id="" >
-            <option value="under one">under one</option>
-            <option value="one to three">one to three</option>
-            <option value="older than three">older than three</option>
-          </select>
-        </div>
-        <div className="adoption-filter">
-          <label htmlFor="">Gender</label>
-          <select name="" id="" >
-            <option value="male">male</option>
-            <option value="female">female</option>
-            <option value="any">any</option>
-          </select>
-        </div>
-        <div className="adoption-filter">
-          <label htmlFor="">Color</label>
-          <select name="" id="" >
-            <option value="grey">grey</option>
-            <option value="white">white</option>
-            <option value="orange">orange</option>
-            <option value="black">black</option>
-          </select>
-        </div>
-        </div>
-        <div className="adoption-filter-result">
-          <a href='/services/adopt/00124' className="adoption-pet">
-            <img src={mochi} alt="" />
-            <p>Mochi <span>00124</span></p>
-          </a>
-          <a href='/services/adopt/00124' className="adoption-pet">
-            <img src={mochi} alt="" />
-            <p>Mochi <span>00124</span></p>
-          </a >
-        <a href='/services/adopt/00124' className="adoption-pet">
-          <img src={mochi} alt="" />
-          <p>Mochi <span>00124</span></p>
-        </a >
-        <a href='/services/adopt/00124' className="adoption-pet">
-          <img src={mochi} alt="" />
-          <p>Mochi <span>00124</span></p>
-        </a >
-        <a href='/services/adopt/00124' className="adoption-pet">
-          <img src={mochi} alt="" />
-          <p>Mochi <span>00124</span></p>
-        </a >
-        <a href='/services/adopt/00124' className="adoption-pet">
-          <img src={mochi} alt="" />
-          <p>Mochi <span>00124</span></p>
-        </a >
-        <a href='/services/adopt/00124' className="adoption-pet">
-          <img src={mochi} alt="" />
-          <p>Mochi <span>00124</span></p>
-        </a >
-        <a href='/services/adopt/00124' className="adoption-pet">
-          <img src={mochi} alt="" />
-          <p>Mochi <span>00124</span></p>
-        </a >
-        <a href='/services/adopt/00124' className="adoption-pet">
-          <img src={mochi} alt="" />
-          <p>Mochi <span>00124</span></p>
-        </a >
-        <a href='/services/adopt/00124' className="adoption-pet">
-          <img src={mochi} alt="" />
-          <p>Mochi <span>00124</span></p>
-        </a >
-        <a href='/services/adopt/00124' className="adoption-pet">
-          <img src={mochi} alt="" />
-          <p>Mochi <span>00124</span></p>
-        </a >
-        <a href='/services/adopt/00124' className="adoption-pet">
-          <img src={mochi} alt="" />
-          <p>Mochi <span>00124</span></p>
-        </a >
-        <a href='/services/adopt/00124' className="adoption-pet">
-          <img src={mochi} alt="" />
-          <p>Mochi <span>00124</span></p>
-        </a >
-        <a href='/services/adopt/00124' className="adoption-pet">
-          <img src={mochi} alt="" />
-          <p>Mochi <span>00124</span></p>
-        </a >
-        <a href='/services/adopt/00124' className="adoption-pet">
-          <img src={mochi} alt="" />
-          <p>Mochi <span>00124</span></p>
-        </a >
-        <a href='/services/adopt/00124' className="adoption-pet">
-          <img src={mochi} alt="" />
-          <p>Mochi <span>00124</span></p>
-        </a >
-        <a href='/services/adopt/00124' className="adoption-pet">
-          <img src={mochi} alt="" />
-          <p>Mochi <span>00124</span></p>
-        </a >
         
+        <div className="adoption-filter-result">
+          {pet.map((d, index) => {
+            return <a key={index} href='/services/adopt/00124' className="adoption-pet">
+              <img src={d.pet_image} alt="" />
+              <p>{d.pet_name}<span>{d.pet_id}</span></p>
+            </a>
+          })}
         </div>
     </div>
   )
